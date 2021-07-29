208cf4fceea0fd6fa5f1af9828b41e72090f53e1
Laure-Anais Giowachini <giowachini.la@gmail.com>
2021-07-29 23:06:26 +0200

    Mentorat - work on valid password

--- controllers/user.js
+++ controllers/user.js
@@ -2,8 +2,34 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
+
+// Crypto sha512 
+// let hash = CryptoJS.SHA512("Message");
+
+// Crypto ou CryptoJS??
+let CryptoJS = require("crypto");
+
+let sha512 = CryptoJS.createHash('sha512').update('Projet6Piment').digest('hex');
+console.log(sha512); 
+
+const passwordValidator = require('password-validator');
+
+var passwordSchema = new passwordValidator();
+
+// Add properties to it
+passwordSchema
+ .is().min(8)                                    // Minimum length 8
+ .is().max(100)                                  // Maximum length 100
+ .has().uppercase()                              // Must have uppercase letters
+ .has().lowercase()                              // Must have lowercase letters
+ .has().digits(2)                                // Must have at least 2 digits
+ .has().not().spaces()                           // Should not have spaces
+ .is().not().oneOf(['Passw0rd', 'Password123']); // Blacklist these values
+ 
+
 exports.signup = (req, res, next) => {
-  bcrypt.hash(req.body.password, 10)
+  if(passwordSchema.validate(req.body.password)){
+    bcrypt.hash(req.body.password, 10)
     .then(hash => {
       const user = new User({
         email: req.body.email,
@@ -15,7 +41,11 @@ exports.signup = (req, res, next) => {
           res.status(400).json({ error })});
     })
     .catch(error => res.status(500).json({ error }));
+  } else {error => res.status(500).json({ error })
+  //  throw{error : 'mot de passe invalide'}
+  }  
 };
+
 
 exports.login = (req, res, next) => {
   User.findOne({ email: req.body.email })
